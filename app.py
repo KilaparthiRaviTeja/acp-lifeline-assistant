0aa14d39d837e985d88e6a57e3b67412ab39676f
Kilaparthi Ravi Teja <142376333+KilaparthiRaviTeja@users.noreply.github.com>
2025-04-27 15:08:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -274,4 +274,3 @@ if st.session_state.step in ['awaiting_confirmation', 'awaiting_provider_switch'
             chat_bubble(user_input, sender='user')
             bot_reply(user_input)
             st.experimental_rerun()
-
